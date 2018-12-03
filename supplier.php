83d3a670d3fa9c80d225e5ecb4a1f39e63f8955f
DylPro <43591910+DylPro@users.noreply.github.com>
2018-12-03 17:12:25 -0500

    Update supplier.php

--- supplier.php
+++ supplier.php
@@ -3,27 +3,27 @@
 
 	<head>
       <title>Supplier Page</title>
+	  <link rel="stylesheet" href="main.css">
 	</head>
 
 <body>
-
 <div align="center">
 <?php include('Navigation.html'); ?>
-<h1>About the Project</h1>
-<p>
-For our database project, we have decided to build a retail store.
-</p>
-
-<div style="min-height:900px; background-color: lightblue;" align="center">
-<br><br><br><br>
+<br>
 <p>Choose the function to either add, delete, update, or display info of our suppliers. <br> </p>
-<table>
 <tr>
 <td><a href="insert.php" style="color:blue;font-weight:bold;">Insert </a> &nbsp&nbsp&nbsp&nbsp;</td>
 <td><a href="delete.php" style="color:blue;font-weight:bold;">Delete </a> &nbsp&nbsp&nbsp&nbsp;</td>
 <td><a href="update.php" style="color:blue;font-weight:bold;">Update </a> &nbsp&nbsp&nbsp&nbsp;</td>
 <td><a href="display.php" style="color:blue;font-weight:bold;">Display</a> &nbsp&nbsp&nbsp&nbsp;</td>
 </tr>
+<table>
+<?php
+		require("public_html/dbconnect.php");	  
+		require("public_html/tableshow.php");
+				
+		show_supplier($conn);
+		?>
 
 </div>
 </body> </html>
