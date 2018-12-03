f800067d5a36b690931020ad53905471d766fe94
DylPro <43591910+DylPro@users.noreply.github.com>
2018-12-03 16:48:58 -0500

    Update supplier.php

--- supplier.php
+++ supplier.php
@@ -6,6 +6,7 @@
 	</head>
 
 <body >
+<?php include('Navigation.html'); ?>
 <div style="min-height:900px; background-color: lightblue;" align="center">
 <br><br><br><br>
 <p>Choose the function to either add, delete, update, or display info of our suppliers. <br> </p>
@@ -16,12 +17,6 @@
 <td><a href="update.php" style="color:blue;font-weight:bold;">Update </a> &nbsp&nbsp&nbsp&nbsp;</td>
 <td><a href="display.php" style="color:blue;font-weight:bold;">Display</a> &nbsp&nbsp&nbsp&nbsp;</td>
 </tr>
-
-</table>
-<br><br><br><br>
-<table>
-<tr><td><image src="Picture.png" width="150" height="100"></td></tr>
-</table>
 
 </div>
 </body> </html>
