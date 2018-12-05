c1e748a417959a1985a26ce6c6c3c4bb6aa03bb8
Mark Gardner <snwbrds2922@Marks-MacBook-Air.local>
2018-12-05 16:53:44 -0500

    added refund stuff

--- refunds.php
+++ refunds.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
-<html> 
+<html>
 
-	<head>
-      <title>Supplier Page</title>
-	  <link rel="stylesheet" href="main.css">
-	</head>
+<head>
+<title>Supplier Page</title>
+<link rel="stylesheet" href="main.css">
+</head>
 
 <body>
 <div align="center">
 <?php include('Navigation.html'); ?>
 <br>
-<p>Choose the function to either add, delete, update, or display info of our suppliers. <br> </p>
+<p>Choose the function to either create, delete, or display info for refunds. <br> </p>
 <tr>
-<td><a href="supplier_insert.php" style="color:blue;font-weight:bold;">Insert </a> &nbsp&nbsp&nbsp&nbsp;</td>
-<td><a href="supplier_delete.php" style="color:blue;font-weight:bold;">Delete </a> &nbsp&nbsp&nbsp&nbsp;</td>
-<td><a href="update.php" style="color:blue;font-weight:bold;">Update </a> &nbsp&nbsp&nbsp&nbsp;</td>
-<td><a href="display.php" style="color:blue;font-weight:bold;">Display</a> &nbsp&nbsp&nbsp&nbsp;</td>
+<td><a href="order_insert.php" style="color:blue;font-weight:bold;">Create </a> &nbsp&nbsp&nbsp&nbsp;</td>
+<td><a href="order__delete.php" style="color:blue;font-weight:bold;">Delete </a> &nbsp&nbsp&nbsp&nbsp;</td>
+<td><a href="order_display.php" style="color:blue;font-weight:bold;">Display</a> &nbsp&nbsp&nbsp&nbsp;</td>
 </tr>
 <table>
 <?php
-		require("public_html/dbconnect.php");	  
-		require("public_html/tableshow.php");
-				
-		show_supplier($conn);
-		?>
+    require("public_html/dbconnect.php");
+    require("public_html/tableshow.php");
+    
+    show_refunds($conn);
+    ?>
 
 </div>
 </body> </html>
