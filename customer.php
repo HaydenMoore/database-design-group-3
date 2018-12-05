ca34f05222c07e4725e473c753706fa5499db679
Max Kotlan <max.maxkot@gmail.com>
2018-12-05 17:02:47 -0500

    modified customer page

--- customer.php
+++ customer.php
@@ -1,48 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html> 
 
-   <head>
-		<title>Update InstructorSalary in MySQL Database</title>
-		<link rel="stylesheet" href="main.css">
-   </head>
+	<head>
+      <title>Employee Page</title>
+	  <link rel="stylesheet" href="main.css">
+	</head>
 
-   <body>
-   <div align="center">
-      <?php include('Navigation.html'); ?>
-      <?php
-
+<body>
+<div align="center">
+<?php include('Navigation.html'); ?>
+<br>
+<p>Here is the list of our inventory. Make additions, deletions, or updates when needed. <br> </p>
+<tr>
+<td><a href="customer_insert.php" style="color:blue;font-weight:bold;">Insert </a> &nbsp&nbsp&nbsp&nbsp;</td>
+<td><a href="customer_delete.php" style="color:blue;font-weight:bold;">Delete </a> &nbsp&nbsp&nbsp&nbsp;</td>
+<td><a href="customer_update.php" style="color:blue;font-weight:bold;">Update </a> &nbsp&nbsp&nbsp&nbsp;</td>
+</tr>
+<table>
+<?php
 		require("public_html/dbconnect.php");	  
 		require("public_html/tableshow.php");
 				
 		show_customer($conn);
-		show_supplier($conn);
-
 		?>
-	  <br><br><br><br>
-     <p>Enter Instructor information for update <br> </p>
-      <form method = "post" action = "<?php $_PHP_SELF ?>">
-         <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
-            <tr>
-               <td width = "250">ID</td>
-               <td>
-                  <input name = "i_ID" type = "text" id = "i_ID">
-               </td>
-            </tr>
 
-            <tr>
-               <td width = "250"></td>
-               <td> </td>
-            </tr>
-         
-            <tr>
-               <td width = "250"> </td>
-               <td>
-                  <input name = "update" type = "submit" id = "update"  value = "update">
-               </td>
-            </tr>
-			
-         </table>
-   </div>
-   
-   </body>
-</html>
+</div>
+</body> </html>
